0fc63da2bd825697368c8e6ee4fd6fd19baa0ae1
spancemoon <duan@hust.edu.cn>
2018-10-08 09:59:54 +0800

    use GET to simu logging

--- demo.py
+++ demo.py
@@ -4,10 +4,12 @@ import urllib2
 
 #values = {"username":"","password":""}
 values = {}
-values["username"] = "duan@hust.edu.cn"
+values["username"] = "18986108575"
 values["password"] = "@dQQ1365710661"
 data = urllib.urlencode(values)
-url = "https://passport.csdn.net/account/login?from=http://my.csdn.net/my/mycsdn"
-request = urllib2.Request(url,data)
+#url = "https://passport.csdn.net/account/login?from=http://my.csdn.net/my/mycsdn"
+url = "http://passport.csdn.net/account/login"
+geturl = url + "?" + data
+request = urllib2.Request(geturl)
 response = urllib2.urlopen(request)
 print response.read()
